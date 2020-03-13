f3ee215dea96e41f7f80ffccdfea2ccbcd0a0d93
Grant Barland <gbarland@UTI.Local>
2020-03-13 14:57:24 -0500

    normalized cut / RAG

--- NoiseShader/NoiseShader.py
+++ NoiseShader/NoiseShader.py
@@ -1,10 +1,34 @@
+"""
+==============
+Normalized Cut
+==============
+
+This example constructs a Region Adjacency Graph (RAG) and recursively performs
+a Normalized Cut on it [1]_.
+
+References
+----------
+.. [1] Shi, J.; Malik, J., "Normalized cuts and image segmentation",
+       Pattern Analysis and Machine Intelligence,
+       IEEE Transactions on, vol. 22, no. 8, pp. 888-905, August 2000.
+"""
+
 import os
-from skimage import io, data_dir
+from skimage import io, data, segmentation, color
+from skimage.future import graph
 from matplotlib import pyplot as plt
-from skimage.transform import rescale, resize
 
-os.chdir('C:\\Art\\NoiseShader')
-img = io.imread('images\\gjb.jpg')
 
+script_dir = os.path.dirname(__file__)
+rel_path = "images/obama.jpg"
+file_path = os.path.join(script_dir, rel_path)
+
+img = io.imread(file_path)
 plt.imshow(img)
+plt.show()
+
+labels1 = segmentation.slic(img, compactness=30, n_segments=500)
+out1 = color.label2rgb(labels1, img, kind='avg')
+
+plt.imshow(out1)
 plt.show()
